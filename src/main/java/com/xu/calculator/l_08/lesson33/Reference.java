8b7a3a75048ed34fd552dc91f7ff1eb28ca8975b
xuchaofeng <826639991@qq.com>
2021-08-05 09:46:45 +0800

    change token

---
+++ src/main/java/com/xu/calculator/l_08/lesson33/Reference.java
@@ -0,0 +1,15 @@
+package com.xu.calculator.l_08.lesson33;
+
+/**
+ * java_study
+ */
+public class Reference {
+    public static void main(String[] args) {
+        int a = 1;
+        add(a);
+    }
+
+    private static void add(int a) {
+        System.out.println(a + 1);
+    }
+}
